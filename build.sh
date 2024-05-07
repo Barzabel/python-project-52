55aaabb0f6fd55b6f7bffce283e4867eb7088fb0
Barzabel <Kochenko1995@mail.ru>
2024-05-07 07:34:22 +0300

    fix render buil

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-# Exit on error
-set -o errexit
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+
 make install
-# Convert static asset files
+
 python manage.py collectstatic --no-input
 python manage.py migrate
