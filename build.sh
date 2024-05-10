b3114b4e5c4bdeec99f753a40cbc828d260b7ffd
Barzabel <Kochenko1995@mail.ru>
2024-05-10 10:28:01 +0300

    fix bash

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ make install
 poetry run python manage.py collectstatic --no-input
 poetry run python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  poetry run python manage.py createsuperuser --no-input
-fi
+# if [[ $CREATE_SUPERUSER ]];
+# then
+#   poetry run python manage.py createsuperuser --no-input
+# fi
