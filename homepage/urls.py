a97439cc93ee5cb13a714894fada9f1eca41ccb7
Barzabel <Kochenko1995@mail.ru>
2024-05-11 13:58:49 +0300

    add users

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,17 +1,27 @@
 from django.urls import path
 from homepage import views
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
+    path('users/', views.UseersView.as_view(), name='users'),
+    path('users/create/', views.RegisterUser.as_view(), name='singup'),
     path('login/', views.LoginUser.as_view(), name='login'),
-
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
 
 
 """
-/users/
-/login/
-/users/create/
+GET /users/ — страница со списком всех пользователей
+GET /users/create/ — страница регистрации нового пользователя
+POST /users/create/ — создание пользователя
+GET /users/<int:pk>/update/ — страница редактирования пользователя
+POST /users/<int:pk>/update/ — обновление пользователя
+GET /users/<int:pk>/delete/ — страница удаления пользователя
+POST /users/<int:pk>/delete/ — удаление пользователя
+GET /login/ — страница входа +
+POST /login/ — аутентификация (вход) +
+POST /logout/ — завершение сессии (выход) ?
 """
--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib import messages
+from django.urls import reverse, reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from .models import User, Label, Status, Task, TaskLabels
 from django.contrib.auth.views import LoginView
-from .forms import LoginUserForm
+from django.views.generic.base import TemplateView
+from django.views.generic import CreateView
+from .forms import LoginUserForm, RegisterUserForm
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -19,10 +22,33 @@ class IndexView(View):
         )
 
 
+
+class UseersView(TemplateView):
+    template_name = "users/users.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users_list'] = User.objects.all()
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = super().get(request, *args, **kwargs)
+        messages_ = messages.get_messages(request)
+        context['messages'] = messages_
+        return context
+
+
+
 class LoginUser(LoginView):
     form_class = LoginUserForm
     template_name = 'users/login.html'
-    extra_context = {'title': 'Авторизация'}
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/singup.html'
+    success_url = reverse_lazy('login')
+
 
 
 # create users
