848b037db8cd5d185be7c18a7389a0e285017607
Barzabel <Kochenko1995@mail.ru>
2024-05-12 19:54:38 +0300

    fix setings

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
