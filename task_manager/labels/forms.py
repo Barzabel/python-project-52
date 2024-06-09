98c29685a4d3d5093f89585ec145638c58214656
Barzabel <Kochenko1995@mail.ru>
2024-06-09 10:55:21 +0300

    fix form label text

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CreateLabelForm(forms.ModelForm):
-
+    name = forms.CharField(max_length=150, required=True, label=_("Name"))
     class Meta:
         model = Label
         fields = ('name', )
--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -6,6 +6,14 @@ from django.utils.translation import gettext_lazy as _
 
 class CreateTaskForm(forms.ModelForm):
 
+
     class Meta:
         model = Task
         fields = ('name', 'body', 'status', 'executor', 'labels')
+        labels = {
+            'name': _('name'),
+            'body': _('body'), 
+            'status': _('status'),
+            'executor': _('executor'),
+            'labels': _('labels')
+        }
